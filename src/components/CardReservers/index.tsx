16ab78bd7c05d3d81457efa1bbc196967f0587eb
MaysharaF <mayshara@alunos.utfpr.edu.br>
2021-08-23 10:34:42 -0300

    Adicionando informações ao card de reservas

--- src/components/CardReservers/index.tsx
+++ src/components/CardReservers/index.tsx
@@ -1,10 +1,39 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
+import { Icon } from "react-native-elements";
+import { theme } from "../../global/styles/theme";
 
 import { styles } from "./styles";
 
 const CardReservers: React.FC = () => {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.nameArea}>Salão de Jogos</Text>
+      <View style={styles.infoUser}>
+        <Text style={styles.nameUser}>Mayshara Fernandes</Text>
+        <Text style={styles.nameUser}>Nº 102</Text>
+      </View>
+      <Text style={styles.dateEvent}>
+        <Icon
+          name="watch"
+          size={15}
+          color={theme.colors.gray_200}
+          style={{ marginRight: 5 }}
+        />
+        24 de Junho de 2021 - 14:00 às 16:30
+      </Text>
+      <Text style={styles.dateEvent}>
+        <Icon
+          name="people"
+          size={15}
+          color={theme.colors.gray_200}
+          style={{ marginRight: 5 }}
+        />
+        5 pessoas
+      </Text>
+      <Text style={styles.statusEvent}>EM ABERTO</Text>
+    </View>
+  );
 };
 
 export default CardReservers;
